f41265f79abf07fb5bf909d1d56b74fa94e0b188
j-glich <72561315+j-glich@users.noreply.github.com>
2021-09-14 16:34:11 -0500

    Correcciones radio butons

--- vista/DatosEscolares.php
+++ vista/DatosEscolares.php
@@ -3,31 +3,86 @@
 <link rel="stylesheet" href="../vista/estilos/s_Escolares.css" />
 <script src="../vista/js/js_Escolares.js"></script>
 <!--INICIO del cont principal-->
+<style>
+
+#control{
+    width: 100%;
+    height: auto;
+    background:none;
+}
+.form{
+    position: relative;
+}
+.textbox{
+    width: 80%;
+    height: 45px;
+    border: 1px solid black;
+    border-radius: 5px;
+    outline: none;
+    background: none;
+    padding: 10px;
+    margin:auto;
+    color: black;
+    left: 35px;
+    position: relative;
+}
+.form_label{
+    position: absolute;
+    top: 0.6rem;
+    pointer-events: none;
+    left: 3.4rem;
+    cursor: text;
+    transition: top 100ms, let 100ms, font-size 100ms ease-in;
+    opacity: 0.5;
+}
+.textbox:focus{
+    border: 2px solid #00aae4;
+}
+.textbox:focus ~ .form_label,
+.textbox:not(:placeholder-shown).textbox:not(:focus)~.form_label
+    {
+    top: -0.7rem;
+    left: 2.7rem;
+    font-size: 15px;
+    background: white;
+    color: black;   
+    opacity: 1;
+
+}
+
+
+</style>
 <div class="container" id='container'>
     <h1 style="text-align: center;">Ficha de identificación</h1>
     <h3 style="text-align: center;">Datos Escolares</h3>
-    <form  method = 'post' action= 'DatosGenSalud.php' >
+    <form class="form" method = 'post' action= 'DatosGenSalud.php' >
         <section class="form-register">
             <div id='cam1'>
                 <div id ='esc'>
                     <label class='label'>Escolaridad:</label><br>
-                    <label class='label'>(</label><input  type='radio' id='radio1' name='grupo' value="Preparatoria" onclick="Eliminar('id_')"><label class='label'>) Preparatoria</label><br>
-                    <label class='label'>(</label><input  type='radio' id='radio2' name='grupo' value="Bachillerato General" onclick="Eliminar('id_')"><label class='label'>) Bachillerato General</label><br>
+                    <label class='label'>(</label><input  type='radio' id='radio1' name='grupo' value="Preparatoria" onclick="delete1('id_','id_eti')"><label class='label'>) Preparatoria</label><br>
+                    <label class='label'>(</label><input  type='radio' id='radio2' name='grupo' value="Bachillerato General" onclick=" delete1('id_','id_eti')"><label class='label'>) Bachillerato General</label><br>
                     <label class='label'>(</label><input  type='radio' id='radio3' name='grupo' value="Bachillerato Técnico" onclick='crearInput()'><label class='label'>) Bachillerato Técnico</label><br>
-                    <label class='label'>(</label><input  type='radio' id='radio4' name='grupo' value="Tele bachillerato" onclick="Eliminar('id_')"><label class='label'>) Tele bachillerato</label><br>
-                    <label class='label'>(</label><input  type='radio' id='radio5' name='grupo' value="Preparatoria Abierta" onclick="Eliminar('id_')"><label class='label'>) Preparatoria Abierta</label><br>
+                    <label class='label'>(</label><input  type='radio' id='radio4' name='grupo' value="Tele bachillerato" onclick=" delete1('id_','id_eti')"><label class='label'>) Tele bachillerato</label><br>
+                    <label class='label'>(</label><input  type='radio' id='radio5' name='grupo' value="Preparatoria Abierta" onclick=" delete1('id_','id_eti')"><label class='label'>) Preparatoria Abierta</label><br>
                     </div>
-                            <label class ='label'>Nombre del instituto de procedencia: </label> 
-        <input class="controls" type="text" name="nomInstitucion" id="nomInstitucion" placeholder='Institucion '>
-        <br>
-        <label class ='label'>Promedio general: </label>
-        <input class="controls" type="text" name="promedio" id="promedio" placeholder="Promedio">
-    </div>    
+                    <br> 
+                <div id="control" class="form">
+                    <input class="textbox" type="text" name="nomInstitucion" id="nomInstitucion" placeholder=' ' autocomplete="off">
+                    <label class="form_label">Nombre del instituto de procedencia:</label>
+                </div>
+                <br>
+                <div id="control" class="form">
+                <input class="textbox" type="text" name="promedio" id="promedio" placeholder=" " autocomplete="off" >
+                    <label class="form_label">Promedio general:</label>
+                </div>
+                <br>
+            </div>    
         <div id='cam2'> 
             <div id ='esc1'>
             <label class ='label'>Has estado becado: </label><br>
-            <label class='label'>(</label><input  type='radio' id='radio6' name='gru' value="si" onclick="habilitar('radio8','radio9','radio10','radio11')" ><label class='label'>) Si</label>  &nbsp &nbsp &nbsp
-            <label class='label'>(</label><input  type='radio' id='radio7' name='gru' value="no" onclick="desahabilitar('radio8','radio9','radio10','radio11','id_beca')"  ><label class='label'>) No</label><br>
+            <label class='label'>(</label><input  type='radio' id='radio6' name='gru' value="SI" onclick="habilitar('radio8','radio9','radio10','radio11')" ><label class='label'>) Si</label>  &nbsp &nbsp &nbsp
+            <label class='label'>(</label><input  type='radio' id='radio7' name='gru' value="NO" onclick="desahabilitar('radio8','radio9','radio10','radio11','id_beca')"  ><label class='label'>) No</label><br>
             <label class='label'>Tipo de beca:</label><br>
             <label class='label'>(</label><input  type='radio' id='radio8' disabled='true' value="Federal" name='grup' onclick="Eliminar1('id_beca')"><label class='label'>) Federal</label><br>
             <label class='label'>(</label><input  type='radio' id='radio9' disabled='true' value="Estatal" name='grup' onclick="Eliminar1('id_beca')"><label class='label'>) Estatal</label><br>
